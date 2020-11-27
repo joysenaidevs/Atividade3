c44396c594202ec3b239812e598b9415e0342838
Joyce <joycessilva17@hotmail.com>
2020-11-26 23:45:21 -0300

    Atividade 3 concluida com auxilio

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace atv_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 3");
+
+            Console.WriteLine("Digite quais são os produtos: ");
+            string nomeProduto = Console.ReadLine();
+
+            Console.WriteLine("Digite a quantidade de produtos: ");
+            int quantidadeProduto = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o preço unitario: ");
+            float precoUnitario = float.Parse(Console.ReadLine());
+            
+            
+            TotalAPagar(quantidadeProduto, precoUnitario);
+
+            float TotalAPagar(int quantidade, float preco)
+            {
+                float total = quantidade * preco;
+                float desconto;
+
+                if(quantidade <=5){
+                    desconto = 0.02f;
+                }else if(quantidade <=10){
+                    desconto = 0.03f;
+                }else{
+                    desconto = 0.05f;
+                }
+                float valorDesconto = total * desconto;
+                float totalDesconto = total - valorDesconto;
+
+                Console.WriteLine($"O valor total é de: {total}");
+                Console.WriteLine($"Voce terá {valorDesconto} de desconto");
+                Console.WriteLine($"Sua compra com desconto fica  {totalDesconto}");
+
+                return totalDesconto;
+
+            } // Fim total a pagar
+
+        } // fim do main
+    }
+}
